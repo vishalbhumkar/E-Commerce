49b8f71b075b7fe3468383395cd515623b4231b4
Vishal Bhumkar <vishalbhumkar12@gmail.com>
2024-12-31 22:25:50 +0530

    Delete updateCartValue.js

--- updateCartValue.js
+++
@@ -1,5 +0,0 @@
-const cartValue = document.querySelector("#cartValue");
-
-export const updateCartValue = (cartProducts) => {
-  return (cartValue.innerHTML = ` <i class="fa-solid fa-cart-shopping"> ${cartProducts.length} </i>`);
-};
